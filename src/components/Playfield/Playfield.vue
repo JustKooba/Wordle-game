14df3c21ca16e69067b87e23f2f4cd805984f95d
JustKooba <jakubsakw2009@gmail.com>
2023-03-07 18:57:15 +0100

    implemented game logic

--- src/components/Playfield/Playfield.vue
+++ src/components/Playfield/Playfield.vue
@@ -1,83 +1,61 @@
 <template>
   <div class="board">
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
-    <div class="square"></div>
+    <div
+      class="square"
+      v-for="(square, index) in 30"
+      :key="index"
+      v-html="currentLetter"
+    ></div>
   </div>
   <div class="keyboard">
-    <div class="key" @click="keyPress">A</div>
-    <div class="key" @click="keyPress">B</div>
-    <div class="key" @click="keyPress">C</div>
-    <div class="key" @click="keyPress">D</div>
-    <div class="key" @click="keyPress">E</div>
-    <div class="key" @click="keyPress">F</div>
-    <div class="key" @click="keyPress">G</div>
-    <div class="key" @click="keyPress">H</div>
-    <div class="key" @click="keyPress">I</div>
-    <div class="key" @click="keyPress">J</div>
-    <div class="key" @click="keyPress">K</div>
-    <div class="key" @click="keyPress">L</div>
-    <div class="key" @click="keyPress">M</div>
-    <div class="key" @click="keyPress">N</div>
-    <div class="key" @click="keyPress">O</div>
-    <div class="key" @click="keyPress">P</div>
-    <div class="key" @click="keyPress">Q</div>
-    <div class="key" @click="keyPress">R</div>
-    <div class="key" @click="keyPress">S</div>
-    <div class="key" @click="keyPress">T</div>
-    <div class="key" @click="keyPress">U</div>
-    <div class="key" @click="keyPress">V</div>
-    <div class="key" @click="keyPress">W</div>
-    <div class="key" @click="keyPress">X</div>
-    <div class="key" @click="keyPress">Y</div>
-    <div class="key" @click="keyPress">Z</div>
-    <div class="key">BACK</div>
-    <div class="key">ENTER</div>
+    <div
+      class="key"
+      v-for="letter in alphabet"
+      :key="letter"
+      @click="keyPress(letter)"
+    >
+      {{ letter }}
+    </div>
+    <div class="key" @click="keyPress('BACK')">BACK</div>
+    <div class="key disabled" @click="keyPress('ENTER')">ENTER</div>
   </div>
 </template>
-
 <script>
 export default {
-  name: "Board",
   data() {
     return {
-      key: "",
+      squares: Array(30).fill(""),
+      position: 0,
+      alphabet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      currentLetter: "",
     };
   },
   methods: {
-    keyPress() {
-      this.key = event.target.innerText;
-      console.log("key pressed: " + this.key);
+    keyPress(letter) {
+      console.log("key pressed: " + letter);
+      this.currentLetter = letter;
+      if (this.position < 30) {
+        this.squares, this.position, this.currentLetter;
+        this.position++;
+      }
+
+      if (
+        this.position === 5 ||
+        this.position === 10 ||
+        this.position === 15 ||
+        this.position === 20 ||
+        this.position === 25
+      ) {
+        this.$el.querySelectorAll(".key").forEach((key) => {
+          if (key.innerText === "ENTER") {
+            key.classList.remove("disabled");
+          } else {
+            key.classList.add("disabled");
+          }
+        });
+      }
     },
   },
 };
 </script>
-
 <style></style>
